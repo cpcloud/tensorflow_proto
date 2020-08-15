ab2029833cf71b005e0e5c46dcf83586a519ec6e
Phillip Cloud <cloud@standard.ai>
2020-08-15 19:25:40 -0400

    Add for desering with options

--- src/lib.rs
+++ src/lib.rs
@@ -22,20 +22,39 @@ pub fn into_bytes(msg: impl prost::Message) -> Result<Vec<u8>, Error> {
 
 #[cfg(test)]
 mod tests {
-    use super::{into_bytes, tensorflow};
+    use crate::{into_bytes, tensorflow};
 
-    #[test]
-    fn test_serde() {
-        let config_proto = tensorflow::ConfigProto {
-            gpu_options: Some(tensorflow::GpuOptions {
-                allow_growth: true,
+    #[cfg(feature = "serde-derive")]
+    mod serde {
+        use crate::tensorflow;
+
+        #[test]
+        fn test_serde_roundtrip() {
+            let config_proto = tensorflow::ConfigProto {
+                gpu_options: Some(tensorflow::GpuOptions {
+                    allow_growth: true,
+                    ..Default::default()
+                }),
                 ..Default::default()
-            }),
-            ..Default::default()
-        };
-        let js = serde_json::to_string(&config_proto).unwrap();
-        let result: tensorflow::ConfigProto = serde_json::from_str(&js).unwrap();
-        assert_eq!(config_proto, result);
+            };
+            let js = serde_json::to_string(&config_proto).unwrap();
+            let result: tensorflow::ConfigProto = serde_json::from_str(&js).unwrap();
+            assert_eq!(config_proto, result);
+        }
+
+        #[test]
+        fn test_deser_with_defaults() {
+            let config_proto = tensorflow::ConfigProto {
+                gpu_options: Some(tensorflow::GpuOptions {
+                    allow_growth: true,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            };
+            let js = r#"{"gpu_options": {"allow_growth": true}}"#;
+            let result: tensorflow::ConfigProto = serde_json::from_str(js).unwrap();
+            assert_eq!(config_proto, result);
+        }
     }
 
     #[test]
